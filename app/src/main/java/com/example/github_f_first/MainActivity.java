d02def70f6e2ac511955fdba2c19540fd63c2e00
Y750-beep <55013125+Y750-beep@users.noreply.github.com>
2019-09-08 08:45:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/github_f_first/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.github_f_first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
